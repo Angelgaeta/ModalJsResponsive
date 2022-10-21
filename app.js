1f9d784cda250f34fdcf0c802bda9019b661bf9b
Angel R <93774378+Angelgaeta@users.noreply.github.com>
2022-10-21 21:27:03 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var cancel = document.getElementsByClassName("annuler")[0],
+  modal = document.getElementsByClassName("popup-cont")[0],
+  confirm = document.getElementsByClassName("valider")[0];
+
+function x() {
+  modal.style.display = "none";
+}
+cancel.onclick = x;
+
+confirm.onclick = function () {
+  modal.style.display = "block";
+};
+
+window.onclick = function (e) {
+  if (e.target.className === "popup-cont") {
+    e.target.style.display = "none";
+  }
+};
